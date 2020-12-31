025d9bbd383ee7a1f8e5c7937811a8bd00c9bbfd
JaeHongMin94 <moonshin123@naver.com>
2020-12-31 19:17:59 +0900

    변수의 타입2

---
+++ ex04.js
@@ -0,0 +1,16 @@
+/*
+  다음 변수를 typeof()로 넣었을 때 출력될 값과의 연결이 알맞지 않은 것은?
+  1) 입력: a = 1, 출력: number
+  2) 입력: b = 2.22, 출력: boolean
+  3) 입력: c = 'p', 출력: string
+  4) 입력: d = [1, 2, 3], 출력: object
+*/
+// b = number
+const a = 1;
+const b = 2.22;
+const c = 'p';
+const d = [1, 2, 3];
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
