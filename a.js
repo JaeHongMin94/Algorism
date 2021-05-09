55f7cb767d25868b737377de9eb7dece47c75ef5
JaeHongMin94 <moonshin123@naver.com>
2021-05-09 15:48:26 +0900

    709. To Lower Case

--- a.js
+++ a.js
@@ -1,34 +1,5 @@
-var countMatches = function (items, ruleKey, ruleValue) {
-  let count = 0;
-  const type = 0,
-    color = 1,
-    name = 2;
-  for (let i = 0; i < items.length; i++) {
-    if (ruleKey === 'type') {
-      if (items[i][type] === ruleValue) {
-        count++;
-      }
-    } else if (ruleKey === 'color') {
-      if (items[i][color] === ruleValue) {
-        count++;
-      }
-    } else {
-      if (items[i][name] === ruleValue) {
-        count++;
-      }
-    }
-  }
-  return count;
+var toLowerCase = function (str) {
+  return str.toLowerCase();
 };
 
-console.log(
-  countMatches(
-    [
-      ['phone', 'blue', 'pixel'],
-      ['computer', 'silver', 'lenovo'],
-      ['phone', 'gold', 'iphone'],
-    ],
-    'color',
-    'silver'
-  )
-);
+console.log(toLowerCase('Hello'));
---
+++ leetCode/toLowerCase.md
@@ -0,0 +1,24 @@
+# 709. To Lower Case
+
+## 문제
+
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+### Example
+
+1. Input: "Hello"  
+   Output: "hello"
+
+2. Input: "here"  
+   Output: "here"
+
+3. Input: "LOVELY"  
+   Output: "lovely"
+
+### 문제 풀이
+
+```js
+var toLowerCase = function (str) {
+  return str.toLowerCase();
+};
+```
